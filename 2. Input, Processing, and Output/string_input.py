96ea53cceac506b16a877cc60bee8f4f6999c3b3
kibetamos <amos.rotich@strathmore.edu>
2023-11-29 17:21:50 +0300

    Add variable reassignment

---
+++ 2. Input, Processing, and Output/string_input.py
@@ -0,0 +1,9 @@
+#Get the first name
+first_name = input("Whats your first name: ")
+
+#get last_name
+last_name = input("Whats your last name: ")
+
+#print a greetng to the user
+
+print('Hello', first_name, last_name)
