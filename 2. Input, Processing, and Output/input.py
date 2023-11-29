e75917698891321766a46a85bc5b203bce8c1be6
kibetamos <amos.rotich@strathmore.edu>
2023-11-29 17:28:51 +0300

    Add variable reassignment

--- 2. Input, Processing, and Output/input.py
+++ 2. Input, Processing, and Output/input.py
@@ -3,5 +3,12 @@
 # variable = input(prompt)
 
 name = input('What is your name? ')
+age = input('Whats your age ')
+income = float(input('Whats is your income '))
 
-print(name)
+
+#Display data
+print('Here is the data you entered')
+print('Name:', name)
+print('Age: ',age)
+print('Income: ',income)
