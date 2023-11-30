76753e9df722b407e20c1a44b765f77253ed88cf
kibetamos <amos.rotich@strathmore.edu>
2023-11-30 19:38:14 +0300

    Update : added simple math

--- 2. Input, Processing, and Output/sale_price.py
+++ 2. Input, Processing, and Output/sale_price.py
@@ -1,3 +1,10 @@
 #we get items original price
 #give a 20 % discount and show the sell price
 
+discount = 20/100
+original_price = input('Input the original price of the product')
+
+selling_price = original_price * discount 
+
+
+print('The price of the product is ', selling_price)
