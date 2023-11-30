fad4dd1a4da1d78ebb58293e25fbe7eba146b1cf
kibetamos <amos.rotich@strathmore.edu>
2023-11-30 20:02:22 +0300

    Update : added time convertor

--- 2. Input, Processing, and Output/time_converter.py
+++ 2. Input, Processing, and Output/time_converter.py
@@ -2,7 +2,15 @@
 
 seconds = float(input('Enter number of seconds'))
 
-# Get a number of seconds from the user.total_seconds = float(input('Enter a number of seconds: ')# Get the number of hours.hours = total_seconds // 360# Get the number of remaining minutes.minutes = (total_seconds // 60) % 6
+# Get a number of seconds from the user.total_seconds = float(input('Enter a number of seconds: ')
+# # Get the number of hours.
+
+
+hours = seconds // 360
+
+# Get the number of remaining minutes.
+# 
+minutes = (seconds // 60) % 6
 # Get the number of remaining seconds.
 seconds = seconds % 60
 # Display the results.
