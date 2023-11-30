fe2db00e8978b3f2f25eb285fd57bc52de76607e
kibetamos <amos.rotich@strathmore.edu>
2023-11-30 19:51:24 +0300

    Update : added simple math

---
+++ 2. Input, Processing, and Output/test_score.py
@@ -0,0 +1,13 @@
+#lets take inputs of an exam 
+math = input('What did you score in Math')
+
+
+eng = input('What did you score in English')
+
+swa = input('What did you score in Swahili')
+
+sci = input('What did you score in Science')
+
+average = math+eng+swa+sci/4
+
+Print('Your result  score is ', average)
