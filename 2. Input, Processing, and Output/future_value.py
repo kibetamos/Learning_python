add899d17db3ea25df9c850c937b44764be48f8d
kibetamos <amos.rotich@strathmore.edu>
2023-12-04 23:26:01 +0300

    Update : added future value

--- 2. Input, Processing, and Output/future_value.py
+++ 2. Input, Processing, and Output/future_value.py
@@ -11,6 +11,7 @@ years = int(input('Enter the number of years the money will grow: '))
 # Calculate the amount needed to deposit.
 present_value = future_value / (1.0 + rate)**years
 
+
  # Display the amount needed to deposit.
 print('You will need to deposit this amount:', present_value)
 
