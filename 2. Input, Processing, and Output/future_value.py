4735c3fc9de104d87ad7dd496cf27311d5835045
kibetamos <amos.rotich@strathmore.edu>
2023-12-04 23:41:36 +0300

    Update : added future value

--- 2. Input, Processing, and Output/future_value.py
+++ 2. Input, Processing, and Output/future_value.py
@@ -2,9 +2,7 @@
 future_value = float(input('Enter the desired future value: '))
 
 # Get the annual interest rate.
-
 rate = float(input('Enter the annual interest rate: '))
-
 # Get the number of years that the money will appreciate.
 years = int(input('Enter the number of years the money will grow: '))
 # Calculate the amount needed to deposit.
