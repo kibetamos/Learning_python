ef2b873447ce943ed5b0031bc5645d862ff9af8c
kibetamos <amos.rotich@strathmore.edu>
2023-12-04 23:40:08 +0300

    Update : added future value

--- 2. Input, Processing, and Output/future_value.py
+++ 2. Input, Processing, and Output/future_value.py
@@ -1,5 +1,6 @@
 # Get the desired future value.
 future_value = float(input('Enter the desired future value: '))
+
 # Get the annual interest rate.
 
 rate = float(input('Enter the annual interest rate: '))
