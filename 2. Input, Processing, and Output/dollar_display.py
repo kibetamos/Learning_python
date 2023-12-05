aa7e836598a8052a2f03578077bb89a1ce6d8617
kibetamos <amos.rotich@strathmore.edu>
2023-12-05 11:29:04 +0300

    Update : added doalr display

---
+++ 2. Input, Processing, and Output/dollar_display.py
@@ -0,0 +1,7 @@
+ # This program demonstrates how a floating-point
+ # number can be displayed as currency.
+monthly_pay = 5000.0
+
+annual_pay = monthly_pay * 12
+
+print(f'Your annual pay is ${annual_pay:,.2f}')
