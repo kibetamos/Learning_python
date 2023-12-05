9d01ec758aac52055f3731e1ad008cd9c94fc3ac
kibetamos <amos.rotich@strathmore.edu>
2023-12-05 11:34:05 +0300

    Update : added doalr display

---
+++ 2. Input, Processing, and Output/center_align.py
@@ -0,0 +1,15 @@
+# This program demonstrates how to center-align strings.
+name1 = 'Gordon'
+name2 = 'Smith'
+name3 = 'Washington'
+name4 = 'Alvarado'
+name5 = 'Livingston'
+name6 = 'Jones'
+
+# Display the names.
+print(f'***{name1:^20}***')
+print(f'***{name2:^20}***')
+print(f'***{name3:^20}***')
+print(f'***{name4:^20}***')
+print(f'***{name5:^20}***')
+print(f'***{name6:^20}***')
