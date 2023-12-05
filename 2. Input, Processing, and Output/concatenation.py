5bc976dd74ab8dc7dc5f667b3d4094cd6463bb87
kibetamos <amos.rotich@strathmore.edu>
2023-12-05 11:12:28 +0300

    Update : added concatenation

---
+++ 2. Input, Processing, and Output/concatenation.py
@@ -0,0 +1,10 @@
+# lets learn string concatanation
+
+first_name = input("What is your name ?")
+second_name =input("What is your second name a,")
+
+#lets join them 
+
+name = first_name + ' '+ second_name
+
+print("Your name is ", name)
