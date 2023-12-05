bd05a73410a2310b4adcfcac6ceda1bac1f65361
kibetamos <amos.rotich@strathmore.edu>
2023-12-05 11:05:38 +0300

    Update : added future value

--- 2. Input, Processing, and Output/test_score.py
+++ 2. Input, Processing, and Output/test_score.py
@@ -1,4 +1,10 @@
 #lets take inputs of an exam 
+
+# 1. Get the first test score.
+# 2. Get the second test score.
+# 3. Get the third test score.
+# 4. Calculate the average by adding the three test scores and dividing the sum by 3.
+# 5. Display the averag
 math = float(input('What did you score in Math '))
 eng = float(input('What did you score in English '))
 swa = float(input('What did you score in Swahili '))
