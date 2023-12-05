dfc9ee0ece23de63019e30c839aebc253f8aaa7f
kibetamos <amos.rotich@strathmore.edu>
2023-12-05 13:28:15 +0300

    Update : added doalr display

---
+++ 2. Input, Processing, and Output/sample.py
@@ -0,0 +1,36 @@
+#What will the following code display?
+name = 'Karlie'
+print('Hello {name}')
+#2.29 What will the following code display?
+name = 'Karlie'
+print(f'Hello {name}')
+#2.30 What will the following code display?
+value = 99
+print(f'The value is {value + 1}')
+#2.31 What will the following code display?
+value = 65.4321
+print(f'The value is {value:.2f}')
+#2.32 What will the following code display?
+value = 987654.129
+print(f'The value is {value:,.2f}')
+#2.33 What will the following code display?
+value = 9876543210
+print(f'The value is {value:,d}')
+#2.34 In the following statement, what is the purpose of the number 10 in the format
+#specifier?
+print(f'{name:10}')
+#2.35 In the following statement, what is the purpose of the number 15 in the format
+#specifier?
+# print(f'{number:15,d}')
+# #2.36 In the following statement, what is the purpose of the number 8 in the format
+# #specifier?
+# print(f'{number:8,.2f}')
+# #2.37 In the following statement, what is the purpose of the < character in the format
+# #specifier?
+# print(f'{number:<12d}')
+# #2.38 In the following statement, what is the purpose of the > character in the format
+# #specifier?
+# print(f'{number:>12d}')
+# #2.39 In the following statement, what is the purpose of the ^ character in the format
+# #specifier?
+# print(f'{number:^12d}')
