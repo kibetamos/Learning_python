cb59bffd719b03d446d50ad6a107778d54ed3041
kibetamos <amos.rotich@strathmore.edu>
2023-12-07 19:10:50 +0300

    added Decision structures and boolean logic

---
+++ 3. Decision Structures/sort_names.py
@@ -0,0 +1,18 @@
+#lets compare strings
+
+#lets get two inputs from the user
+
+name1 = input("Enter your name (Last name first)")
+name2 = input("Enter another name (last name first)")
+
+
+#lest show the names
+print("Here are the names, Listed alphabetically")
+
+if name1 < name2:
+    print (name1)
+    print(name2)
+
+else:
+    print(name2)
+    print(name1)
