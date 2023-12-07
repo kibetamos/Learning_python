82a18d5a16d0b6ecf2e3165547a5f979536fb859
kibetamos <amos.rotich@strathmore.edu>
2023-12-07 18:23:02 +0300

    added Decision structures and boolean logic

--- 3. Decision Structures/password.py
+++ 3. Decision Structures/password.py
@@ -5,6 +5,9 @@ password = input('Enter the password: ')
  # Determine whether the correct password
  # was entered.
 if password == 'prospero':
- print('Password accepted.')
+
+    print('Password accepted.')
+    
 else:
+ 
  print('Sorry, that is the wrong password.')
