c9c7b8d8fc1ad680f24a45662c17b9fa920ca035
kibetamos <amos.rotich@strathmore.edu>
2023-12-08 09:31:30 +0300

    added Decision structure - loan qualifier

---
+++ 3. Decision Structures/Nested/loan_qualifier.py
@@ -0,0 +1,11 @@
+#This programme determines whether a bank customer qualifies for a loan
+
+min_salary = 20000
+min_year = 2
+
+#lets get customers annual salary
+salary = float(input('Enter your annual salary: '))
+
+#number of years on the current job
+
+year_on_job = int(input('Enter the number of ' + 'years employed: '))
