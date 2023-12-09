422133f2c25c4080edfb3b19c5741e9bc490c51e
kibetamos <amos.rotich@strathmore.edu>
2023-12-09 23:31:55 +0300

    added Repitition structures

--- 4.Repetition Structures/commission.py
+++ 4.Repetition Structures/commission.py
@@ -1,1 +1,10 @@
 #calculate sales commissions
+
+#lets create a variable to contol the loop
+keep_going = 'y'
+
+#calculate a series of commissions
+while keep_going == 'y':
+    # Get a salesperson's sales and commission rate
+    sales = float(input('Enter the amount of sales: '))
+    comm_rate = float(input('Enter the commission rate: '))
