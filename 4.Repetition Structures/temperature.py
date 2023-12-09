3dcfd4aa95fbeb0194f5394276a585958a46e310
kibetamos <amos.rotich@strathmore.edu>
2023-12-09 23:52:32 +0300

    added Repitition structures

--- 4.Repetition Structures/temperature.py
+++ 4.Repetition Structures/temperature.py
@@ -1,7 +1,6 @@
 # This program assists a technician in the process
 # of checking a substance's temperature.
 
- # Named constant to represent the maximum
- # temperature.
+ # Named constant to represent the maximum temperature.
 
 max_temp = 102.5
