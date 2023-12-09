9bff42e15162b7ff9d5dff6f928807da605924b3
kibetamos <amos.rotich@strathmore.edu>
2023-12-10 00:11:56 +0300

    added Repitition structures

--- 4.Repetition Structures/temperature.py
+++ 4.Repetition Structures/temperature.py
@@ -10,3 +10,16 @@ max_temp = 102.5
 temperature = float(input("Enter the substance's Celsius temperature: "))
 
 # As long as necessary, instruct the user to # adjust the thermostat.
+
+while temperature > max_temp:
+    print('The temperature is too high.')
+    print('Turn the thermostat down and wait')
+    print('5 minutes. Then take the temperature')
+    print('again and enter it.')
+    temperature = float(input('Enter the new Celsius temperature: '))
+
+
+    # Remind the user to check the temperature again
+# in 15 minutes.
+print('The temperature is acceptable.')
+print('Check it again in 15 minutes.')
