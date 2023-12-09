28ca45d416e036bfa1fac65203548f57666c4785
kibetamos <amos.rotich@strathmore.edu>
2023-12-09 23:44:14 +0300

    added Repitition structures

--- 4.Repetition Structures/commission.py
+++ 4.Repetition Structures/commission.py
@@ -8,3 +8,17 @@ while keep_going == 'y':
     # Get a salesperson's sales and commission rate
     sales = float(input('Enter the amount of sales: '))
     comm_rate = float(input('Enter the commission rate: '))
+
+    # Calculate the commission.
+    commission = sales * comm_rate
+
+    # Display the commission
+    print(f'The commission is ${commission:,.2f}.')
+
+
+    # See if the user wants to do another one.
+    keep_going = input('Do you want to calculate another ' + 'commission (Enter y for yes): ')
+
+
+else:
+    print("Kongoi")
