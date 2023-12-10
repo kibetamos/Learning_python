9c6c4d54f33660b74bdeec073766e462d78ae29a
kibetamos <amos.rotich@strathmore.edu>
2023-12-10 23:30:57 +0300

    added Repitition structures

--- 4.Repetition Structures/simple_loop1.py
+++ 4.Repetition Structures/simple_loop1.py
@@ -1,6 +1,6 @@
-# This program demonstrates a simple for loop
- # that uses a list of numbers.
+# This program demonstrates a simple for loop that uses a list of numbers.
 
 print('I will display the numbers 1 through 5.')
 for num in [1, 2, 3, 4, 5]:
+
     print(num)
