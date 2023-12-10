b914f4941f8c5587040477c65f201ab4cedef8d3
kibetamos <amos.rotich@strathmore.edu>
2023-12-10 22:25:57 +0300

    added Repitition structures

--- 4.Repetition Structures/infinite.py
+++ 4.Repetition Structures/infinite.py
@@ -1,3 +1,18 @@
 #lets demonstrate an infinite loop
 
 #lets create a variable to control the loop
+
+keep_going = 'y'
+
+# Warning! Infinite loop!
+while keep_going == 'y':
+
+# Get a salesperson's sales and commission rate.
+    sales = float(input('Enter the amount of sales: '))
+    comm_rate = float(input('Enter the commission rate: '))
+
+ # Calculate the commission.
+    commission = sales * comm_rate
+
+# Display the commission.
+print(f'The commission is ${commission:,.2f}.')
