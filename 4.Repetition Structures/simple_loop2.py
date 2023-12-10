0881f9322afe46ce68248c0cbea250f09b6e75d7
kibetamos <amos.rotich@strathmore.edu>
2023-12-10 23:31:38 +0300

    added Repitition structures

---
+++ 4.Repetition Structures/simple_loop2.py
@@ -0,0 +1,6 @@
+1 # This program also demonstrates a simple for
+2 # loop that uses a list of numbers.
+3
+4 print('I will display the odd numbers 1 through 9.')
+5 for num in [1, 3, 5, 7, 9]:
+6 print(num)
