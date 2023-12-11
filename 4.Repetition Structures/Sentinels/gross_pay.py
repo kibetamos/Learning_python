87a796c0dcdfe3018e84256eb3c7a4034297b07f
kibetamos <amos.rotich@strathmore.edu>
2023-12-11 19:40:44 +0300

    added Repitition structures

---
+++ 4.Repetition Structures/Sentinels/gross_pay.py
@@ -0,0 +1,12 @@
+ # This program displays gross pay.
+# Get the number of hours worked.
+hours = int(input('Enter the hours worked this week: '))
+
+ # Get the hourly pay rate.
+pay_rate = float(input('Enter the hourly pay rate: '))
+
+ # Calculate the gross pay.
+gross_pay = hours * pay_rate
+
+# Display the gross pay.
+print(f'Gross pay: ${gross_pay:,.2f}')
