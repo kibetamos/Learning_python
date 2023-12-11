ad8b292054cbbbe97bbf77d4b79ff4a81f8d4411
kibetamos <amos.rotich@strathmore.edu>
2023-12-11 15:00:38 +0300

    added Repitition structures

--- 4.Repetition Structures/sum_numbers.py
+++ 4.Repetition Structures/sum_numbers.py
@@ -7,4 +7,14 @@ total = 0.0
 #explanation
 print('This program calculates the sum of ' , end='')
 
-print(f'{max} numbers you will enter')
+print(f'{max} numbers you will enter. ')
+
+#get the numbers and accumulate them
+
+for counter in range(max):
+    number = int(input('Enter a number: '))
+    total = total + number
+
+    #lets show the total of the numbers
+
+    print(f'The total is {total}. ')
