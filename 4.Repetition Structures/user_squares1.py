8e0bc309a59f7ce016fedc02b77ed9fe653c6321
kibetamos <amos.rotich@strathmore.edu>
2023-12-11 14:25:26 +0300

    added Repitition structures

--- 4.Repetition Structures/user_squares1.py
+++ 4.Repetition Structures/user_squares1.py
@@ -1,2 +1,13 @@
 #get users a loop to display a table of numbers and theirs sqaures
 
+# Get the ending limit.
+print('This program displays a list of numbers')
+print('(starting at 1) and their squares.')
+
+end = int(input('How high should I go? '))
+
+# print the number and theri squares
+
+for number in range(1, end + 1):
+    square = number **2
+    print(f'{number}\t{square}')
