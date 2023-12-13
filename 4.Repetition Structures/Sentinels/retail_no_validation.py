1b6bee70e964ff188ce10a25d5ae168d4308543b
kibetamos <amos.rotich@strathmore.edu>
2023-12-13 17:53:49 +0300

    added Repitition structures

--- 4.Repetition Structures/Sentinels/retail_no_validation.py
+++ 4.Repetition Structures/Sentinels/retail_no_validation.py
@@ -6,4 +6,14 @@ another = 'y' # Variable to control the loop
 # Process one or more items.
 while another == 'y' or another == 'Y':
     wholesale = float(input("Enter the item's wholesale cost: "))
-    
+
+    #lets calculate the retail price
+
+    retail = wholesale * MARK_UP
+
+    #display the retail price
+
+    print(f'Retail price: ${retail:,.2f}')
+
+    #lets do it again
+    another = input('Do you have another item? ' +'(Enter y for yes): ')
