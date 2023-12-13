178f63eeec3eab1c5169b082207f65032630575a
kibetamos <amos.rotich@strathmore.edu>
2023-12-13 17:51:32 +0300

    added Repitition structures

--- 4.Repetition Structures/Sentinels/retail_no_validation.py
+++ 4.Repetition Structures/Sentinels/retail_no_validation.py
@@ -4,3 +4,6 @@ MARK_UP = 2.5 # The markup percentage
 another = 'y' # Variable to control the loop
 
 # Process one or more items.
+while another == 'y' or another == 'Y':
+    wholesale = float(input("Enter the item's wholesale cost: "))
+    
