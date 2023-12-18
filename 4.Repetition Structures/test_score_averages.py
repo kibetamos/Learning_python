1bacc19684e3cde394db5a3de90e7c18a81a6574
kibetamos <amos.rotich@strathmore.edu>
2023-12-18 22:29:24 +0300

    added Repitition structures

--- 4.Repetition Structures/test_score_averages.py
+++ 4.Repetition Structures/test_score_averages.py
@@ -1,2 +1,4 @@
 # This program averages test scores. It asks the user for the
 # number of students and the number of test scores per student.
+
+num_students = int(input('How many students do you have? '))
