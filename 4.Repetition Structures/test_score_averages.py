21c329f728a2ad4d7923eae0009ed913d53d6404
kibetamos <amos.rotich@strathmore.edu>
2023-12-18 22:52:11 +0300

    added Repitition structures

--- 4.Repetition Structures/test_score_averages.py
+++ 4.Repetition Structures/test_score_averages.py
@@ -7,3 +7,10 @@ num_students = int(input('How many students do you have? '))
 
 # Get the number of test scores per student.
 num_test_scores = int(input('How many test scores per student? '))
+
+# Determine each student's average test score.
+for student in range(num_students):
+    total = 0.0
+    print(f'Student number {student + 1}')
+
+    print('-----------------')
