031fca682afd0650216f8217e219612c46f6ea23
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 16:21:31 +0300

    added Running total

--- 4.Repetition Structures/RunningTotal/sum_numbers.py
+++ 4.Repetition Structures/RunningTotal/sum_numbers.py
@@ -1,3 +1,10 @@
 #Calculate the sum of seriers of numbers entered by the user]]
 
 
+max = 5
+
+total = 0.0
+
+# Explain what we are doing.
+print('This program calculates the sum of ', end='')
+print(f'{max} numbers you will enter.')
