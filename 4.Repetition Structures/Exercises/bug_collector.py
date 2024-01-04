ee8705e2776a0026ae6c5f72d7263edffdb94438
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 17:22:59 +0300

    added : Exercise 1 Bug collcetor

--- 4.Repetition Structures/Exercises/bug_collector.py
+++ 4.Repetition Structures/Exercises/bug_collector.py
@@ -1,3 +1,12 @@
+#PROBLEM STATEMENT
+'''
+1. Bug Collector
+A bug collector collects bugs every day for five days. Write a program that keeps a running
+total of the number of bugs collected during the five days. The loop should ask for the
+number of bugs collected for each day, and when the loop is finished, the program should
+display the total number of bugs collected.
+
+'''
 total = 0
 max = 5
 
