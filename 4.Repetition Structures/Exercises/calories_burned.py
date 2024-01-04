a360fcfd8ce4b60ac40befe3b06756e6a38e0ec9
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 17:45:21 +0300

    Added calories Burned assignemnt

--- 4.Repetition Structures/Exercises/calories_burned.py
+++ 4.Repetition Structures/Exercises/calories_burned.py
@@ -1,3 +1,8 @@
+
+'''
+Running on a particular treadmill you burn 4.2 calories per minute. Write a program that
+uses a loop to display the number of calories burned after 10, 15, 20, 25, and 30 minutes.
+'''
 calories_burned_per_minute = 4.2
 
 for minutes in range(10, 31, 5):
