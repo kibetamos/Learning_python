f81839e4717086bdd6f071879e021d3f8285cf2e
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 17:14:31 +0300

    added : Exercise 1 Bug collcetor

--- 4.Repetition Structures/Exercises/bug_collector.py
+++ 4.Repetition Structures/Exercises/bug_collector.py
@@ -1,6 +1,12 @@
 total = 0
 max = 5
 
+
+
+# Explain what we are doing.
+print('This program calculates the sum of ', end='')
+print(f'{max} numbers you will enter.') 
+
 for counter in range(max):
     number = int(input('Enter a number: '))
 
