3afb240041f5683a5755ff10e7a521e27fad116d
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 17:27:50 +0300

    added : Exercise 1 Bug collcetor

--- 4.Repetition Structures/Exercises/bug_collector.py
+++ 4.Repetition Structures/Exercises/bug_collector.py
@@ -11,11 +11,11 @@ total = 0
 max = 5
 
 # Explain what we are doing.
-print('This program calculates the total number of bugs collected ', end='')
-# print(f'{max} numbers you will enter.') 
+print(f'This program calculates the total number of bugs collected in ', end='')
+print(f'{max} days you will enter.') 
 
 for counter in range(max):
-    number = int(input('Enter number of bugs collected: '))
+    number = int(input('Enter the number of bugs collected: '))
 
     total = total+ number
 
