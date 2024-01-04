4d0d350c442afc3a2a2bbd0fd1c331428053a8e8
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 17:10:15 +0300

    added Running total

---
+++ 4.Repetition Structures/Exercises/bug_collector.py
@@ -0,0 +1,11 @@
+total = 0
+max = 5
+
+for counter in range(max):
+    number = int(input('Enter a number: '))
+
+    total = total+ number
+
+
+#display sum of the numbers
+print(f'The total is {total}')
