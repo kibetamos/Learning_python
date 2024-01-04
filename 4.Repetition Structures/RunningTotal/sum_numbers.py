bf655a1eef79e00b4735f7c3dcbb30e953d5eee9
kibetamos <amos.rotich@strathmore.edu>
2024-01-04 16:28:15 +0300

    added Running total

--- 4.Repetition Structures/RunningTotal/sum_numbers.py
+++ 4.Repetition Structures/RunningTotal/sum_numbers.py
@@ -8,3 +8,15 @@ total = 0.0
 # Explain what we are doing.
 print('This program calculates the sum of ', end='')
 print(f'{max} numbers you will enter.')
+
+#get the number and accumukate them 
+
+for counter in range(max):
+    number = int(input('Enter a number: '))
+
+    total = total+ number
+
+
+#display sum of the numbers
+    
+print(f'The total is {total}')
