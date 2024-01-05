3c75ca0e340d489148d2fdcd44f04deedd561806
kibetamos <amos.rotich@strathmore.edu>
2024-01-05 09:12:50 +0300

    Added budget analysis  assignemnt

--- 4.Repetition Structures/Exercises/budget_analysis.py
+++ 4.Repetition Structures/Exercises/budget_analysis.py
@@ -1,8 +1,8 @@
 # 3. Budget Analysis
-
 '''
 Write a program that asks the user to enter the amount that he or she has budgeted for
 a month. A loop should then prompt the user to enter each of his or her expenses for the
 month and keep a running total. When the loop finishes, the program should display the
 amount that the user is over or under budget.
 '''
+
