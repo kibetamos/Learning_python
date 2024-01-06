be85669fe49239ea7a2eb5e8c032aa29af8680ea
kibetamos <amos.rotich@strathmore.edu>
2024-01-06 21:54:12 +0300

    Added average  analysis  assignemnt

--- 4.Repetition Structures/Exercises/average_rainfall.py
+++ 4.Repetition Structures/Exercises/average_rainfall.py
@@ -1,1 +1,9 @@
 # Average Rainfall 
+'''
+Write a program that uses nested loops to collect data and calculate the average rainfall over
+a period of years. The program should first ask for the number of years. The outer loop will
+iterate once for each year. The inner loop will iterate twelve times, once for each month.
+Each iteration of the inner loop will ask the user for the inches of rainfall for that month.
+After all iterations, the program should display the number of months, the total inches of
+rainfall, and the average rainfall per month for the entire period.
+'''
