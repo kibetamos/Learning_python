c7f6af9552983fe0302ac44ff1724ecf75f777ae
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 14:44:34 +0300

    Added Functions sample

---
+++ 5.Functions/cups_to_ounces.py
@@ -0,0 +1,22 @@
+#lets convert cupt to ounces
+
+def main():
+    #lets deisplay the introduction to the screen
+    intro()
+    cups = int(input('Enter the number of cups'))
+    cups_to_ounces(cups)
+
+#Introduction screen
+def intro():
+    print('This program converts measurements')
+    print('in cups to fluid ounces. For your')
+    print('reference the formula is:')
+    print(' 1 cup = 8 fluid ounces')
+    print()
+
+def cups_to_ounces(cups):
+    ounces = cups * 8
+    print(f'That converts to {ounces} ounces.')
+
+
+main()
