17c3cdfe70870b0cbc4ad080132549e948e616da
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 10:24:32 +0300

    Added Functions sample

--- 5.Functions/acme_dryer.py
+++ 5.Functions/acme_dryer.py
@@ -16,5 +16,30 @@ def main():
 
     #Display step 4
     step4()
+
+
+# The startup_message function displays the
+# program's initial message on the screen.
     
-     
+def startup_message():
+    print('This is how to disassemble an ACME laundry dryer')
+    print('We have 4 steps in the process')
+
+
+#Step 1
+def step1():
+    print('Step 1: Unplug the dryer and move it away from the wall' )
+
+#step 2
+def step2():
+    print('Step 2: Remove the six screws from the back of the dryer')
+
+#step 3
+def step3():
+    print('Step 3: Remove the back panel from th dryer')
+
+#step 4
+def step4():
+    print('Step 4: Pull the top of the dryer straight up.')
+
+main()
