0ec077884527106843afe9356b0db93bb9abeecf
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 10:16:13 +0300

    Added Functions sample

---
+++ 5.Functions/acme_dryer.py
@@ -0,0 +1,20 @@
+#programe to display step by step proces to assemble ACME dryer
+
+def main():
+    startup_message()
+    input('Press Enter to see Step 1 .')
+    #Display ste 1
+    step1()
+    input('Press Enter to see Step 2')
+    #Display step 2
+    step2()
+    input('Press Enter to see Step 3. ')
+
+    #Display step 3
+    step3()
+    input('Press Enter to see Step 4. ')
+
+    #Display step 4
+    step4()
+    
+     
