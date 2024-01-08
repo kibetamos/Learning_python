f6c08be67ffc0d140b14b622a4077d6ba27ef5f5
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 09:27:52 +0300

    Added Functions sample

--- 5.Functions/function1.py
+++ 5.Functions/function1.py
@@ -1,9 +1,11 @@
 
 def funct1():
-    sum = a+b
+    
 
-    a = int(input('Enter a'))
-    b = int(input('Enter b'))
+    a = int(input('Enter a '))
+    b = int(input('Enter b '))
+
+    sum = a + b
 
     print ('The sum is,', sum)
 
