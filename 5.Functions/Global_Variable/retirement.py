4e1cc2ddcdf6b9af63666906b2091f41afa87247
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 17:01:21 +0300

    Added Functions sample

---
+++ 5.Functions/Global_Variable/retirement.py
@@ -0,0 +1,23 @@
+# we have a global constant to represent the contribution rate 
+cont_rate = 0.05
+
+def main():
+    gross_pay = float(input('Enter your gros pay: '))
+    bonus = float(input('Enter the amount of bonuses '))
+    pay_contrib(gross_pay)
+    bonus_contrib(bonus)
+
+#pay_contrib accepts grosspay as an argument
+def pay_contrib(gross):
+    contrib= gross * cont_rate
+
+    print(f'Contribution for gross pay: {contrib}')
+
+
+def bonus_contrib(bonus):
+    contrib = bonus*cont_rate
+
+    print(f'Contribution for bonuses: {contrib}')
+
+
+main()
