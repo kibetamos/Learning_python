b4c3cf433ecd7225299f4ba42a289c1b4c31e79e
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 14:34:07 +0300

    Added Functions sample

--- 5.Functions/pass_arg.py
+++ 5.Functions/pass_arg.py
@@ -1,1 +1,15 @@
 #pass an argument to a function 
+def main():
+    value = 5
+    show_double(value)
+
+#show_value function accets an arguemnt and displays the value
+    
+def show_double(number):
+    result = number * 2
+
+    print(result)
+
+#call main function
+    
+main()
