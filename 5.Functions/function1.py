136c0d998b8e0388cf27b4cd525dab3714464a58
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 09:37:37 +0300

    Added Functions sample

--- 5.Functions/function1.py
+++ 5.Functions/function1.py
@@ -1,12 +1,14 @@
-
+#define a function
 def sum():
-    
+    #get  input from a user
     a = int(input('Enter a '))
-
+#get second input from a user
     b = int(input('Enter b '))
-
+#we call the second function which we have used to calculate the sum
     answer(a,b,sum)
 
+#we define function 2 to do the sum
+    #it takes in 3 parameters a,b and sum
 def answer(a,b,sum):
     sum = a + b
     print ('The sum is,', sum)
