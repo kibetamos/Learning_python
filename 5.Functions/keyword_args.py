21f971b598edcf10585fef918a0d7bba4bbe4000
kibetamos <amos.rotich@strathmore.edu>
2024-01-08 16:30:59 +0300

    Added Functions sample

--- 5.Functions/keyword_args.py
+++ 5.Functions/keyword_args.py
@@ -4,8 +4,11 @@ def main():
     rate=0.01
     period=10
     principal=10000.0
-    show_intrest(rate,period,principal)
 
+    show_intrest(rate,period,principal)
+# The show_interest function displays the amount of
+# simple interest for a given principal, interest rate
+# per period, and number of periods.
 def show_intrest(rate,period,principal):
     intrest = rate*period*principal
 
