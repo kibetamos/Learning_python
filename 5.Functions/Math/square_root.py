9216ba7ec2ea0c451e949edd6afe91b697b43c75
kibetamos <amos.rotich@strathmore.edu>
2024-01-09 17:43:43 +0300

    Added Functions sample

--- 5.Functions/Math/square_root.py
+++ 5.Functions/Math/square_root.py
@@ -1,11 +1,14 @@
 #demonstrates the sqrt function
 import math
 
+
 def main():
+#get a number
     number = int(input("Enter a number "))
-
-
+#lets calculate the squareroot
     square_root = math.sqrt(number)
+   #print the result
     print(f'The square root of {number} is {square_root}') 
 
+#call main function
 main()
