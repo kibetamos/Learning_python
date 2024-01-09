641a99e2c62f1366b589cdca4271d276ec873c48
kibetamos <amos.rotich@strathmore.edu>
2024-01-09 12:20:29 +0300

    Added Functions sample

--- 5.Functions/RandomNumbers/commission_rate.py
+++ 5.Functions/RandomNumbers/commission_rate.py
@@ -1,1 +1,14 @@
 # This program calculates a salesperson's pay # at Make Your Own Music.
+
+def main():
+    sales = get_sales()
+
+    #lets check the mount of advanced pay
+
+    advanced_pay = get_advanced_pay()
+
+def get_sales():
+    pass
+
+def get_advanced_pay():
+    pass
