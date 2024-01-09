2322cefdbf593cb4e129c2c86315f755e4cd47cd
kibetamos <amos.rotich@strathmore.edu>
2024-01-09 12:29:15 +0300

    Added Functions sample

--- 5.Functions/RandomNumbers/commission_rate.py
+++ 5.Functions/RandomNumbers/commission_rate.py
@@ -7,8 +7,18 @@ def main():
 
     advanced_pay = get_advanced_pay()
 
+    #determin e the commison rate
+    comm_rate = determine_comm_rate(sales)
+
+#lets calculate the pay
+    pay = sales* comm_rate - advanced_pay
+#Display the amount of pay
+    print(f'The pay is ${pay:,.2f}.')
+
 def get_sales():
     pass
 
 def get_advanced_pay():
+    pass
+def determine_comm_rate(sale):
     pass
