e649217880d9a1943942771b3c4bba38dc9ef16f
kibetamos <amos.rotich@strathmore.edu>
2024-01-09 12:48:00 +0300

    Added Functions sample

--- 5.Functions/RandomNumbers/commission_rate.py
+++ 5.Functions/RandomNumbers/commission_rate.py
@@ -39,6 +39,19 @@ def get_advanced_pay():
 #The determine_comm_rate function accepts the
  # amount of sales as an argument and returns the
  # applicable commission rate.
-def determine_comm_rate(sale):
-    pass
+def determine_comm_rate(sales):
+    #determine commision rate
+    if sales < 1000.00:
+        rate =0.10
+    elif sales >= 10000 and sales <= 14999.99:
+        rate =0.12
+    elif sales >= 15000 and sales <= 17999.99:
+        rate = 0.14
+    elif sales >= 18000 and sales <= 21999.99:
+        rate = 0.16
+    else:
+        rate = 0.18
+
+    return rate
+
 
