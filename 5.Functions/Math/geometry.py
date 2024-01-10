3455a2e08b57a9c1edc15f53a3ebd3a9904966d2
kibetamos <amos.rotich@strathmore.edu>
2024-01-10 10:02:28 +0300

    Added Functions sample

--- 5.Functions/Math/geometry.py
+++ 5.Functions/Math/geometry.py
@@ -38,6 +38,19 @@ def main():
             length = float(input("Enter the rectangle's length: "))
             print('The area is', rectangle.area(width, length))
 
-            
+        elif choice == perimeter_rectanle_choice:
+            width = float(input("Enter the rectangle's width: "))
+            length = float(input("Enter the rectangle's length: "))
+
+            print('The perimeter is', rectangle.perimeter(width, length))
+
+
+        elif choice == quit_choice:
+
+            print('Exiting the program. . .')
+
+        else:
+
+            print('Error: invalid selection.')
 
 
