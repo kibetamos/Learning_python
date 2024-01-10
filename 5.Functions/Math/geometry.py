c44c11cb05147b45476c25bb9b17c3d1ad3831bf
kibetamos <amos.rotich@strathmore.edu>
2024-01-10 09:47:43 +0300

    Added Functions sample

--- 5.Functions/Math/geometry.py
+++ 5.Functions/Math/geometry.py
@@ -10,3 +10,9 @@ area_rectangle_choice = 3
 perimeter_rectanle_choice = 4
 quit_choice = 5
 
+#main function
+def main():
+    #controls the loop and holds users menu ChildProcessError
+    choice = 0
+    
+
