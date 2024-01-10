b1cf8f505161cfd250c0dbc49ca2cfe3b85a4f63
kibetamos <amos.rotich@strathmore.edu>
2024-01-10 18:01:47 +0300

    Added Functions - Turtles

--- 5.Functions/Turtles/draw_circles.py
+++ 5.Functions/Turtles/draw_circles.py
@@ -3,11 +3,9 @@ import turtle
 def main():
     turtle.hideturtle()
     circle(0, 0, 100, 'red')
-    circle(−150, −75, 50, 'blue')
-    circle(−200, 150, 75, 'green')
-
-
-
+    circle(-150, -75, 50, 'blue')
+    circle(-200, 150, 75, 'green')
+    turtle.done()
 
 # The circle function draws a circle. The x and y parameters
 # are the coordinates of the center point. The radius
@@ -15,11 +13,15 @@ def main():
 # is the fill color, as a string.
 
 def circle(x, y, radius, color):
-    turtle.penup() # Raise the pen
-    turtle.goto(x, y - radius) # Position the turtle
-    turtle.fillcolor(color) # Set the fill color
-    turtle.pendown() # Lower the pen
-    turtle.begin_fill() # Start filling
-    turtle.circle(radius) # Draw a circ
+    turtle.penup()
+    turtle.goto(x, y - radius)
+    turtle.pendown()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
 
-
+    
+# Call the main function.
+if __name__ == '__main__':
+    main()
