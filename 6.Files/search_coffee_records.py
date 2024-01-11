fdf89f82e6d9360d36490abb3d898993528e548c
kibetamos <amos.rotich@strathmore.edu>
2024-01-11 11:17:56 +0300

    Added Files

--- 6.Files/search_coffee_records.py
+++ 6.Files/search_coffee_records.py
@@ -8,3 +8,42 @@ def main():
     #lets get the search value
 
     search = input('Enter a description to search for ...')
+
+    #lets open the file we want to search
+
+    coffee_file = open('coffee.txt', 'r')
+
+    ## Read the first record's description field.
+    descr = coffee_file.readline()
+
+    #lets read the rest of the file
+
+    while descr != '':
+        #read the quantity field
+
+        qty = float(coffee.readline())
+
+        #strip the \n from the descriotion
+
+        descr = descr.rstrip('\n')
+
+        #Determine whether this record matches the search value
+        if descr == search:
+            #display the record
+            print(f'Description: {descr}')
+            print(f'Quantity : {qty}')
+            print()
+
+            #Read the next description
+
+        descr = coffee_file.readline()
+
+    coffee_file.close()
+
+    #if the search value was not found in the display message
+    if not found:
+        print('That item is not found in the file')
+
+#call the main function
+if __name__ == '__main__':
+    main()
