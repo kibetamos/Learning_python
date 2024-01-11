37e134bd1cc72fa4c5c79321f10388db5a919872
kibetamos <amos.rotich@strathmore.edu>
2024-01-11 13:47:15 +0300

    Added search functionality

--- 6.Files/delete_coffee_record.py
+++ 6.Files/delete_coffee_record.py
@@ -17,5 +17,40 @@ def main():
     
     #Read the first records descrition fiel
     descr = coffee_file.readline()
-    
-     
+
+    #lets read the rest of the file
+
+    while descr != '':
+#read quantity field
+        qty = float(coffee_file.readline())
+
+        descr = descr.rstrip('\n')
+
+        if descr != search:
+            temp_file.write(f'{descr}\n')
+            temp_file.write(f'{qty}\n')
+        
+        else:
+            found = True
+
+        #read the next description
+
+    descr = coffee_file.readine()
+
+    coffee_file.close()
+    temp_file.close()
+
+
+    os.remove('coffee.txt')
+
+    os.rename('temp.txt', 'coffee.txt')
+
+    if found:
+        
+        print('The file has been updated.')
+    else:
+        print('That item was not found in the file.')
+
+if __name__ == '__main__':
+
+    main()
