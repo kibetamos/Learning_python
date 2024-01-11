076a5bab608119cfb05dc3edb33543b865d6ed95
kibetamos <amos.rotich@strathmore.edu>
2024-01-11 10:51:44 +0300

    Added Files

---
+++ 6.Files/search_coffee_records.py
@@ -0,0 +1,10 @@
+#this programme allows the user search txt for records matchng the description
+
+def main():
+    #create a bool variable to use a flag
+
+    found = False
+
+    #lets get the search value
+
+    search = input('Enter a description to search for ...')
