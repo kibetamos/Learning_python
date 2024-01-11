0a62a181d368e92bb38ba023a29cc817192a48c6
kibetamos <amos.rotich@strathmore.edu>
2024-01-11 10:16:28 +0300

    Added Files

--- 6.Files/write_sales.py
+++ 6.Files/write_sales.py
@@ -1,9 +1,9 @@
 #prompt user for sales amoints anmd write them in a sales txt file 
 
 def main():
-    #lets get the number of days
 
-    num_days= int(input('For how many days do you have sales'))
+    #lets get the number of days
+    num_days = int(input('For how many days do you have sales'))
 
     #open a new fie names sales.txt
 
@@ -14,11 +14,10 @@ def main():
     sales = float(f'Enter the sales for day #{count}: ')
     sales_file.write(f'{sales}\n')
 
-
     # Close the file.
     sales_file.close()
-    print('Data written to sales.txt.')
 
+    print('Data written to sales.txt.')
 
 if __name__ == '__main__':
     main()
