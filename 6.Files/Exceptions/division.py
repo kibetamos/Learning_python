6ba2a8cf37b7c636f7e1f79c204ebb02deca17d6
kibetamos <amos.rotich@strathmore.edu>
2024-01-12 14:37:43 +0300

    Added modify functionality

---
+++ 6.Files/Exceptions/division.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    #lets get numbers to devide
+    a = int(input('Enter number A: '))
+
+    b = int(input('Enter number b: '))
+
+    divide = a/b
+
+    print(divide)
+
+if __name__ == '__main__':
+    main()
