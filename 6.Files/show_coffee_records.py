d65602af25e75517e678ec3ba80bc0b84073d696
kibetamos <amos.rotich@strathmore.edu>
2024-01-12 13:43:08 +0300

    Added show functionality

--- 6.Files/show_coffee_records.py
+++ 6.Files/show_coffee_records.py
@@ -1,4 +1,24 @@
 # This program displays the records in the
 
 def main():
-    3O
+    coffee_file = open('coffee.txt', 'r')
+
+    descr = coffee_file.readline()
+
+    while descr != '':
+        qty = float(coffee_file.readline())
+
+        #strip tjhe \n fro the sescriptiom
+
+        descr = descr.rstrip('\n')
+
+        #display the record
+        print(f'Description: {descr}')
+        print(f'Quantity: {qty}')
+
+        descr = coffee_file.readline()
+
+    coffee_file.close()
+
+if __name__ == '__main__':
+    main()
