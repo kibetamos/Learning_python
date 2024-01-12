f1f1e07cbafe8c2dea9aab6184ae06c65d812c9e
kibetamos <amos.rotich@strathmore.edu>
2024-01-12 16:08:33 +0300

    Added Exceptions- Gross pay

---
+++ 6.Files/Exceptions/gross_pay2.py
@@ -0,0 +1,21 @@
+#lets calculate th gross pay
+
+def main():
+    try:
+        #lets get payrate
+        pay_rate = float(input('Whats your pay rate '))
+
+        #Lets add hours worked
+
+        hours = int(input('How manuy hours have you worked '))
+
+        #Grosspay
+        gross_pay = hours * pay_rate
+
+        print(f'Your Gross pay is {gross_pay:,.2f}')
+    except ValueError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
