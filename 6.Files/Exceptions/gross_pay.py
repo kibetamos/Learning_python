f2fd7e55fe0b2fbdf7b88aa444f95b225a321c18
kibetamos <amos.rotich@strathmore.edu>
2024-01-12 14:56:31 +0300

    Added Exceptions- Gross pay

--- 6.Files/Exceptions/gross_pay.py
+++ 6.Files/Exceptions/gross_pay.py
@@ -1,19 +1,20 @@
 #lets calculate th gross pay
 
 def main():
+    try:
+        #lets get payrate
+        pay_rate = float(input('Whats your pay rate '))
 
-    #lets get payrate
-    pay_rate = float(input('Whats your pay rate'))
+        #Lets add hours worked
 
-    #Lets add hpours worked
+        hours = int(input('How manuy hours have you worked '))
 
-    hours = int(input('How manuy hours have you worked'))
+        #Grosspay
+        gross_pay = hours * pay_rate
 
-
-    #Grosspay
-    gross_pay = hours * pay_rate
-
-    print(f'Your Gross pay is {gross_pay}')
+        print(f'Your Gross pay is {gross_pay:,.2f}')
+    except ValueError:
+        print('ERROR: Hours worked and hourly pay rate must be valid numbers.')
 
 
 if __name__ == '__main__':
