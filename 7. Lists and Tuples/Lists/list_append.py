a28d25abf675f1db6991dc866afd2ef7bfab2d37
kibetamos <amos.rotich@strathmore.edu>
2024-01-13 14:37:07 +0300

    Added Lists and tuples- append

--- 7. Lists and Tuples/Lists/list_append.py
+++ 7. Lists and Tuples/Lists/list_append.py
@@ -1,3 +1,32 @@
 #Append is to add
 # This program demonstrates how the append
 # method can be used to add items to a list.
+
+def main():
+    #First, create an empty list
+    name_list = []
+
+    again = 'y'
+
+    #create a variable to control the loop
+    while again == 'y':
+        #get a name from the user
+        name = input('Enter a name: ')
+
+        #append the name to the list
+        name_list.append(name)
+
+        #add another ?
+        print('Do you want to add another name ?')
+        again = input('y = yes, anything else = no: ')
+        print()
+
+    for name in name_list:
+        print(name)
+
+#call the main function
+        
+if __name__ == '__main__':
+    main()
+
+        
