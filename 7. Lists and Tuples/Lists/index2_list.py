724de1b9be9832a301e17112e47d61c2d305e1fa
kibetamos <amos.rotich@strathmore.edu>
2024-01-13 15:34:49 +0300

    Added Lists and tuples- append

---
+++ 7. Lists and Tuples/Lists/index2_list.py
@@ -0,0 +1,12 @@
+
+#lets create a list
+comp = ['hp', 'samsung', 'dell', 'Lenovo']
+
+def main():
+    #lets get the list printed out
+    print('Here are your computers')
+
+    print(comp)
+
+if __name__ == '__main__':
+    main()
