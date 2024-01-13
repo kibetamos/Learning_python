aa608f0b6ee7df3bea66b29469c297cc1cb863e5
kibetamos <amos.rotich@strathmore.edu>
2024-01-13 15:39:26 +0300

    Added Lists and tuples- append

--- 7. Lists and Tuples/Lists/index2_list.py
+++ 7. Lists and Tuples/Lists/index2_list.py
@@ -8,5 +8,24 @@ def main():
 
     print(comp)
 
+    #lets get the comuter we want to change
+    item = input('Which computer do you want to change')
+
+    try:
+        item_index = comp.index(item)
+
+        #lets replace it
+        new_comp = input('Whats the new computer')
+
+        comp[item_index] = new_comp
+
+        print('Here is our new comp mlist')
+
+        print(comp)
+
+    except ValueError as err:
+        print(err)
+
+        
 if __name__ == '__main__':
     main()
