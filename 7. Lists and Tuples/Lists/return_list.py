49a01228dc9185f5d8612f553896f139caeb99af
kibetamos <amos.rotich@strathmore.edu>
2024-01-14 15:35:02 +0300

    Added Lists and tuples- append

---
+++ 7. Lists and Tuples/Lists/return_list.py
@@ -0,0 +1,24 @@
+def main():
+    numbers = get_values()
+
+    # Display the values in the list.
+    print('The numbers in the list are:')
+    print(numbers)
+
+def get_values():
+    values = []
+
+    #we have to create a loop and control it 
+    again = 'y'
+
+    #lets get the values from the user
+    while again == 'y':
+        num = int(input('Enter the value '))
+        values.append(num)
+
+        #do you want this again ?
+        print('Do you want to add another number ? ')
+
+        again = input('y = yes, anything else = no: ')
+        print()
+        
