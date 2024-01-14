5b256903036ea3d322420360eb707c6c4f3dc7d4
kibetamos <amos.rotich@strathmore.edu>
2024-01-14 10:22:06 +0300

    Added Lists and tuples- append

--- 7. Lists and Tuples/Lists/total_list.py
+++ 7. Lists and Tuples/Lists/total_list.py
@@ -2,10 +2,13 @@
 
 def main():
     books = [50,2,4,5,8]
+    # Create a variable to use as an accumulator.
+
     total = 0
+    # Calculate the total of the list elements.
     for value in books:
         total += value
-
+# Display the total of the list elements.
     print(f'The total of the elements is {total}.')
 
 if __name__ == '__main__':
