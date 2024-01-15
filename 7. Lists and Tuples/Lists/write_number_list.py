928327b5335038f13e6e4c255fd9607b08eff1fb
kibetamos <amos.rotich@strathmore.edu>
2024-01-15 08:31:22 +0300

    Added Lists and tuples- append

---
+++ 7. Lists and Tuples/Lists/write_number_list.py
@@ -0,0 +1,19 @@
+def main():
+    #create a list of numbers
+
+    numbers = [1,2,3,4,5,6,7]
+
+    #open a file to write to
+    outfile = open('numberlist.txt', 'w')
+
+    #write list to the file
+    for item in numbers:
+        outfile.rite(str(item) + '\n')
+
+    outfile.close()
+
+
+#call main function
+if __name__ == '__main__':
+    main()
+
