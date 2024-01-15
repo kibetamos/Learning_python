f9f531d4ec28aa6d6d7ef91be8f24a896241b235
kibetamos <amos.rotich@strathmore.edu>
2024-01-15 10:25:30 +0300

    Added Exercises

---
+++ 7. Lists and Tuples/Exercises/population.py
@@ -0,0 +1,12 @@
+'''
+If you have downloaded the source code you will find a file named USPopulation.txt
+in the Chapter 07 folder. The file contains the midyear population of the United States, in
+thousands, during the years 1950 through 1990. The first line in the file contains the popu-
+lation for 1950, the second line contains the population for 1951, and so forth.
+Write a program that reads the file’s contents into a list. The program should display the
+following data:
+• The average annual change in population during the time period
+• The year with the greatest increase in population during the time period
+• The year with the smallest increase in population during the time period
+
+'''
