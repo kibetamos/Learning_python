8c8cc1e850709292be4d5ba1634c4b5b3cd05344
kibetamos <amos.rotich@strathmore.edu>
2024-01-25 15:48:53 +0300

    Add namespace

--- Namespace/name.py
+++ Namespace/name.py
@@ -1,1 +1,7 @@
-dir(__builtins__)
+global_var = "I'm in the global namespace"
+
+def my_function():
+    print(global_var)  # Accessing variable from the global namespace
+
+my_function()
+print(global_var)
