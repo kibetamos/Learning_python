3b5b53ba714503084576e165d069658fc6bc4fcc
kibetamos <amos.rotich@strathmore.edu>
2024-01-26 10:57:23 +0300

    Add namespace

---
+++ Namespace/enclosing_namespace.py
@@ -0,0 +1,22 @@
+'''
+We have defined a function insuide a function
+
+outer function -  the main function
+
+inner function - function inside a function
+
+'''
+def outer_function():
+    outer_var = "This is the outer function"
+
+    def inner_function():
+        # Accessing variable from the enclosing namespace
+        print(outer_var)  
+    #call the function
+    inner_function()
+#call the outer function
+outer_function()
+
+# Trying to access outer_var outside the function
+# This will raise a NameError
+# print(outer_var)
