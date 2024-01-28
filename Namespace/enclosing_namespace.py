6f6fec9cff698ae9cf9796e9c8b7c2e04826e3c4
kibetamos <amos.rotich@strathmore.edu>
2024-01-28 07:56:11 +0300

    namespace

--- Namespace/enclosing_namespace.py
+++ Namespace/enclosing_namespace.py
@@ -1,10 +1,7 @@
 '''
-We have defined a function insuide a function
-
+We have defined a function inside a function
 outer function -  the main function
-
 inner function - function inside a function
-
 '''
 def outer_function():
     outer_var = "This is the outer function"
@@ -16,7 +13,9 @@ def outer_function():
     inner_function()
 #call the outer function
 outer_function()
-
-# Trying to access outer_var outside the function
-# This will raise a NameError
-# print(outer_var)
+'''
+Trying to access outer_var outside the function, 
+will raise a NameError
+print(outer_var)
+'''
+print(outer_var)
