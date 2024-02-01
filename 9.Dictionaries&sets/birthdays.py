5c5f45fc69ab930151aaae0e0b74cffb620c5cf3
kibetamos <amos.rotich@strathmore.edu>
2024-02-01 11:30:46 +0300

    Dictionaries and Sets- birthdays

---
+++ 9.Dictionaries&sets/birthdays.py
@@ -0,0 +1,14 @@
+#python programe to keep friends birthdates 
+
+#global constants
+look_up = 1
+add = 2
+change = 3
+delete = 4
+quit = 5
+
+#main function
+def main():
+    birthdaYS = {}
+    
+    
