d2c37731e88b8abb828ba80893d997d8505da2d5
kibetamos <amos.rotich@strathmore.edu>
2024-02-01 18:21:59 +0300

    Added birthdays

--- 9.Dictionaries&sets/birthdays.py
+++ 9.Dictionaries&sets/birthdays.py
@@ -9,6 +9,89 @@ quit = 5
 
 #main function
 def main():
-    birthdaYS = {}
+    birthdays = {}
     
+    # Initialize variable for the users choice
+    choice = 0
+
+    while choice != quit:
+        #Get users menu choice
+
+        choice = get_menu_choice()
+
+        #process the choice
+        if choice == look_up:
+            look_up(birthdays)
+        elif choice == add:
+            add(birthdays)
+        elif choice == change:
+            change(birthdays)
+        elif choice == delete:
+            delete(birthdays)
+        
+# The get_menu_choice function displays the menu
+# and gets a validated choice from the user
+def get_menu_choice():
+    print()
+    print('Friends and Their Birthdays')
+    print('---------------------------')
+    print('1. Look up a birthday')
+    print('2. Add a new birthday')
+    print('3. Change a birthday')
+    print('4. Delete a birthday')
+    print('5. Quit the program')
+
+    print()
+
+
+    #Get the users choice
+
+    choice = int(input('Enter your choice: '))
+
+
+    # Validate the choice.
+    while choice < look_up or choice > quit:
+        choice = int(input('Enter a valid choice: '))
+
+
+    # return the users choice 
+
+    return choice     
+
+def look_up(birthdays):
+    name = input('Enter a name: ')
     
+    #look it upa dictionary
+    print(birthdays.get(name, 'Not found.'))
+
+#the add functionadds a new entry into the birthdays dictioanry
+    
+def add(birthdays):
+    #get name and birthdays
+    name = input('Enter a name: ')
+    bday = input('Enter a birthday: ')
+
+
+    # If the name does not exist, add it.
+    if name not in birthdays:
+        birthdays[name] = bday
+    else:
+        print('That entry already exists.')
+
+
+
+def delete(birthdays):
+    #get name to looup
+    name = input('Enter a name')
+    ## If the name is found, delete the entry.
+
+    if name in birthdays:
+        del birthdays[name]
+    else:
+        print('That name is not found.')
+
+
+#call the main function
+        
+if __name__ == '__main__':
+    main()
